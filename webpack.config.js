7f2cfe188636ec4df0d58857996dc0f91c9c22a4
Matt Keas <matthiasak@gmail.com>
2016-12-04 12:57:15 -0600

    comitting and pushing new patch version

--- webpack.config.js
+++ webpack.config.js
@@ -1,100 +1,47 @@
-const webpack = require("webpack")
-const path = require('path')
-// const Offline = require('offline-plugin')
-const OptimizeJsPlugin = require("optimize-js-plugin")
+let config =
+	require("@terse/webpack")
+	.api()
+	.entry({
+		index: "./src/index.js"
+	})
+	.loader(
+		'babel-loader'
+		, '.js'
+		, {
+			exclude: /node_modules/
+			, query: { cacheDirectory: true, presets: ['stage-0'] }
+		})
+	.plugin("webpack.NamedModulesPlugin")
+	.plugin("optimize-js-plugin")
+	.plugin("webpack.NoErrorsPlugin")
+	.externals(/^@?\w[a-z\-0-9\./]+$/)
+	.output({
+		path: './build'
+		, library: 'clan'
+		, libraryTarget: 'commonjs2'
+	})
+	.target("node")
+	.sourcemap("source-map")
+	.when("production", api => api
+		.plugin("webpack.LoaderOptionsPlugin", {
+			minimize: true,
+			debug: false
+		})
+		.plugin("webpack.optimize.UglifyJsPlugin", {
+			compress: {
+				warnings: true
+			}
+		}))
+	.when("development", api => api
+		.entry({
+			index: "./src/index.js"
+		})
+		.plugin("webpack.LoaderOptionsPlugin", {
+			minimize: false,
+			debug: true
+		}))
+	.getConfig()
 
-// debug?
-const isDebug = process.env.NODE_ENV !== "production"
+delete config.module.preLoaders
 
-const browser = {
-
-    // Developer tool to enhance debugging, source maps
-    // http://webpack.github.io/docs/configuration.html#devtool
-    devtool: isDebug ? 'source-map' : false,
-
-    // Options affecting the normal modules
-    // https://webpack.github.io/docs/list-of-loaders.html
-    module: {
-        loaders: [{
-            exclude: /node_modules/,
-            loader: "babel-loader",
-            query: { cacheDirectory: true, compact: true },
-            test: /\.(jsx|js|es|es6|mjs)$/,
-        }]
-    },
-
-    // http://webpack.github.io/docs/configuration.html#resolve
-    resolve: {
-        alias: {
-            // components: path.resolve('src/components/'),
-        }
-    },
-
-    node: {
-        __filename: true,
-        __dirname: true,
-    },
-
-    output: {
-        devtoolModuleFilenameTemplate: "[absolute-resource-path]",
-        filename: "[name].js",
-        chunkFilename: "[id].js",
-        sourceMapFilename: "[name].js.map",
-        path: './build',
-        library: 'clan',
-        libraryTarget: 'commonjs2'
-        // publicPath: '/build',
-    },
-
-    entry: {
-      "index": "./src/index.js"
-    },
-
-    target: "web",
-
-    plugins: [
-
-        new webpack.LoaderOptionsPlugin({
-            minimize: !isDebug,
-            debug: isDebug
-        }),
-
-        new webpack.NamedModulesPlugin(),
-
-        // new webpack.ProvidePlugin({
-        //     React: 'react',
-        //     DOM: 'react-dom'
-        // }),
-
-        new webpack.DefinePlugin({
-            'process.env':{
-                'NODE_ENV': JSON.stringify(isDebug ? 'production' : 'development')
-            }
-        }),
-
-        // new Offline({
-        //   ServiceWorker: {
-        //     events: true,
-        //     caches: {
-        //       main: ['/', '/**.js'],
-        //     }
-        //   }
-        // }),
-
-    ]
-    .concat(isDebug ? [
-        // new webpack.HotModuleReplacementPlugin
-    ] : [
-        // new OptimizeJsPlugin(),
-        // new webpack.optimize.OccurrenceOrderPlugin,
-        // new webpack.optimize.AggressiveMergingPlugin,
-        new webpack.optimize.UglifyJsPlugin({
-            compress:{
-                warnings: true
-            }
-        }),
-    ])
-
-}
-
-module.exports = {default: browser, isDebug }
+module.exports = config
