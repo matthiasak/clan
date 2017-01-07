cbc525b02d8e51a037e17b242d5519d91542e23b
Matt Keas <matthiasak@gmail.com>
2017-01-07 16:57:33 -0600

    comitting and pushing new patch version

--- fuse.js
+++ fuse.js
@@ -1,54 +1,32 @@
 const f = require("fuse-box")
 	, chokidar = require('chokidar')
 	, dev = process.env.NODE_ENV !== 'production'
+	, closure = require('fusebox-closure-plugin').default
 
 let c = {
 	homeDir: "src/"
 	, cache: dev
 	, package: 'clan-fp'
-	, globals: { default: 'clan-fp' }
+	, globals: { 'clan-fp': 'clan-fp' }
 	, sourceMap: {
 		bundleReference: "index.js.map"
 		, outFile: "./build/index.js.map"
 	}
 	, outFile: "./build/index.js"
-	, inFile: "> index.js [index.js]"
-	, plugins: (browser) =>
-		[
-		f.BabelPlugin({
-			config: {
-				sourceMaps: true
-				, presets: ['es2015-without-strict', 'es2016', 'es2017']
-				// , env: { production: {presets: ['babili'] }}
-				, plugins: ["fast-async"]
-			}
-		})
-		]
+	, inFile: "> index.ts [index.ts]"
+	, plugins: [
+		f.TypeScriptHelpers()
+		, !dev && closure()
+	].filter(x => !!x)
 }
 
-const processAll = $ => {
+const build = $ => {
 	let d = Object.assign({}, c)
 		, inFile = d.inFile
-	d.plugins = d.plugins(!!d.browser)
 	f.FuseBox.init(d).bundle(inFile)
 }
 
-const debounce = (func, wait, immediate, timeout) =>
-	() => {
-		let context = this
-			, args = arguments
-			, later = $ => {
-				timeout = null
-				!immediate && func.apply(context, args)
-			}
-			, callNow = immediate && !timeout
-
-		clearTimeout(timeout)
-		timeout = setTimeout(later, wait)
-		callNow && func.apply(context, args)
-	}
-
-let p = debounce(processAll, 250)
+let p = require('lodash.debounce')(build, 250)
 
 dev &&
 	chokidar
