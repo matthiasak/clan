e658864eaa38d45a1b9796cb20b380610dc8a059
Matt Keas <matthiasak@gmail.com>
2017-01-07 17:01:34 -0600

    comitting and pushing new patch version

--- fuse.js
+++ fuse.js
@@ -13,7 +13,7 @@ let c = {
 		, outFile: "./build/index.js.map"
 	}
 	, outFile: "./build/index.js"
-	, inFile: "> index.ts [index.ts]"
+	, inFile: "> index.ts"
 	, plugins: [
 		f.TypeScriptHelpers()
 		, !dev && closure()
